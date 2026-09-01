af60e6f9e8babb9f083c8ba69e23e8e944cfe2eb
eacosta1956 <eldon.costa@terra.com.br>
2026-09-01 00:49:22 -0300

    feat: criar tela inicial

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 android {
     namespace = "com.eldon.lojavirtualmvvm"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
