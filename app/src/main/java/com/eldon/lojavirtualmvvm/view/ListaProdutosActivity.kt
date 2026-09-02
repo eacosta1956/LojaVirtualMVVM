919b4a0bcfbf2407b9719a80c2e87d8164b889f4
eacosta1956 <eldon.costa@terra.com.br>
2026-09-02 17:43:48 -0300

    feat: exibir lista de produtos

--- app/src/main/java/com/eldon/lojavirtualmvvm/view/ListaProdutosActivity.kt
+++ app/src/main/java/com/eldon/lojavirtualmvvm/view/ListaProdutosActivity.kt
@@ -1,9 +1,11 @@
 package com.eldon.lojavirtualmvvm.view
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.eldon.lojavirtualmvvm.R
 import com.eldon.lojavirtualmvvm.databinding.ActivityListaProdutosBinding
 import com.eldon.lojavirtualmvvm.viewmodel.ListaProdutosViewModel
@@ -13,6 +15,8 @@ class ListaProdutosActivity : AppCompatActivity() {
     private lateinit var binding: ActivityListaProdutosBinding
 
     private val viewModel: ListaProdutosViewModel by viewModels()
+
+    private val adaptadorProdutos = AdaptadorProdutos()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,5 +28,30 @@ class ListaProdutosActivity : AppCompatActivity() {
 
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
+
+        binding.botaoVoltar.setOnClickListener {
+            finish()
+        }
+
+        configurarRecyclerView()
+        observarViewModel()
+
+        viewModel.carregarProdutos()
+    }
+
+    private fun configurarRecyclerView() {
+        binding.recyclerProdutos.layoutManager = LinearLayoutManager(this)
+        binding.recyclerProdutos.adapter = adaptadorProdutos
+    }
+
+    private fun observarViewModel() {
+        viewModel.produtos.observe(this) { produtos ->
+            adaptadorProdutos.atualizarLista(produtos)
+        }
+
+        viewModel.mensagemErro.observe(this) { mensagem ->
+            binding.textoErro.visibility =
+                if (mensagem.isNullOrBlank()) View.GONE else View.VISIBLE
+        }
     }
 }
