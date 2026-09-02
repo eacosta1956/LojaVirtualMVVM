cffbd2c4547859ca6b3311573021f3da091eb435
eacosta1956 <eldon.costa@terra.com.br>
2026-09-02 17:53:53 -0300

    feat: adicionar viewmodel de detalhe do produto

--- app/src/main/java/com/eldon/lojavirtualmvvm/model/ServicoProdutosApi.kt
+++ app/src/main/java/com/eldon/lojavirtualmvvm/model/ServicoProdutosApi.kt
@@ -1,9 +1,15 @@
 package com.eldon.lojavirtualmvvm.model
 
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ServicoProdutosApi {
 
     @GET("products")
     suspend fun buscarProdutos(): RespostaProdutos
+
+    @GET("products/{id}")
+    suspend fun buscarProdutoPorId(
+        @Path("id") idProduto: Int
+    ): Produto
 }
